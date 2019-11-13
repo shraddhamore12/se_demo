d5f9d5767bd0034b1779ea015b2b79f108a36e4a
shraddha <moreshraddhasudhakar@gmail.com>
2019-11-13 14:54:56 +0530

    Inial commit statement

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (request, response) => {
+    response.send('welcome to get')
+})
+
+app.listen(4000, () => {
+    console.log(`Server started on port 4000`);
+});
